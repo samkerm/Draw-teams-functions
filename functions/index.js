be6df01b433070bb53a43af7050f926fb693dcda
Sam Kheirandish <sam.kheirandish@fortify.pro>
2018-02-18 12:35:24 -0800

    Fixed addimg message and receiving the right call back on the app

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,82 @@
+'use strict';
+
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
+const express = require('express');
+const cors = require('cors')({origin: true});
+const app = express();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path /messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into the Realtime Database using the Firebase Admin SDK.
-  return admin.database().ref('/messages').push({original: original}).then((snapshot) => {
-    // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-    return res.redirect(303, snapshot.ref);
+// Express middleware that validates Firebase ID Tokens passed in the Authorization HTTP header.
+// The Firebase ID token needs to be passed as a Bearer token in the Authorization HTTP header like this:
+// `Authorization: Bearer <Firebase ID Token>`.
+// when decoded successfully, the ID Token content will be added as `req.user`.
+const validateFirebaseIdToken = (req, res, next) => {
+  console.log('Check if request is authorized with Firebase ID token');
+
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer ')) {
+    console.error('No Firebase ID token was passed as a Bearer token in the Authorization header.',
+        'Make sure you authorize your request by providing the following HTTP header:',
+        'Authorization: Bearer <Firebase ID Token>',
+        'or by passing a "__session" cookie.');
+    console.log(req.headers.authorization);
+    // console.log();
+    res.status(403).send('Unauthorized succka');
+    return;
+  }
+
+  let idToken;
+  if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+    console.log('Found "Authorization" header');
+    // Read the ID Token from the Authorization header.
+    idToken = req.headers.authorization.split('Bearer ')[1];
+  }
+  admin.auth().verifyIdToken(idToken).then((decodedIdToken) => {
+    console.log('ID Token correctly decoded', decodedIdToken);
+    req.user = decodedIdToken;
+    return next();
+  }).catch((error) => {
+    console.error('Error while verifying Firebase ID token:', error);
+    res.status(403).send('Unauthorized succka');
   });
+};
+
+app.use(cors);
+app.use(validateFirebaseIdToken);
+// app.use(addMessage);
+
+app.get('/hello', (req, res) => {
+  res.send(`Hello ${req.user.name}`);
 });
+
+app.get('/addMessage', (req, res) => {
+  if (req.query && req.query.original) {
+    const original = req.query.original;
+    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+    return admin.database().ref('/messages')
+      .push({original: original})
+      .then(
+        (snapshot) =>
+        {
+          const data = snapshot.key;
+          console.log('Received snapshot: ', data);
+          // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+          return res.send(data);
+        }
+      ).catch(
+        err =>
+        {
+          return res.status(403).send(err);
+        }
+    );
+  }
+  return res.status(403).send('Message content is missing');
+});
+
+// This HTTPS endpoint can only be accessed by your Firebase Users.
+// Requests need to be authorized by providing an `Authorization` HTTP header
+// with value `Bearer <Firebase ID Token>`.
+exports.app = functions.https.onRequest(app);
 
 // Listens for new messages added to /messages/:pushId/original and creates an
 // uppercase version of the message to /messages/:pushId/uppercase
