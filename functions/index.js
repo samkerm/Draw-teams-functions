377e8f74a8cd0fa77c330f04bc5fae021d9ef9e1
Sam Kheirandish <sam.kheirandish@fortify.pro>
2018-04-29 16:31:36 -0700

    Removing the unwanted makeUpperCase function

--- functions/index.js
+++ functions/index.js
@@ -28,19 +28,6 @@ app.use(users.getUserInfo);
 // with value `Bearer <Firebase ID Token>`.
 exports.app = functions.https.onRequest(app);
 
-// Listens for new messages added to /messages/:pushId/original and creates an
-// uppercase version of the message to /messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/messages/{pushId}/original').onWrite((event) => {
-  // Grab the current value of what was written to the Realtime Database.
-  const original = event.data.val();
-  console.log('Uppercasing', event.params.pushId, original);
-  const uppercase = original.toUpperCase();
-  // You must return a Promise when performing asynchronous tasks inside a Functions such as
-  // writing to the Firebase Realtime Database.
-  // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-  return event.data.ref.parent.child('uppercase').set(uppercase);
-});
-
 exports.replicateRatings = functions.database.ref('/users/{userId}/ratings').onWrite((event) => {
   console.log('Database triggered functions');
   console.log(event);
