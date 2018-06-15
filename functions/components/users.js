80dd5e3922a5793bc117008b79f730613d1889f1
Sam Kheirandish <sam.kheirandish@fortify.pro>
2018-06-14 23:10:11 -0700

    Improved user setup

--- functions/components/users.js
+++ functions/components/users.js
@@ -29,26 +29,22 @@ users.initializeUserWithRatings = app.post('/users/initializeUserWithRatings', (
   if (req.body, req.user)
   {
     const body = JSON.parse(req.body);
-
+    let newUser;
     return admin.auth().updateUser(req.user.uid, {
         displayName: body.displayName,
       })
       .then((userRecord) =>
       {
-        console.log('Successfully updated user', userRecord.toJSON());
-        const reference = admin.database().ref('users/' + req.user.uid);
-        return reference.set({
+        newUser = userRecord.toJSON();
+        console.log('Successfully updated user', newUser);
+        return admin.database().ref('users/' + newUser.uid).set({
           displayName: body.displayName,
           ratings: body.ratings,
         });
       })
       .then(() => {
-        console.log('Initialization procesing val');
-        return reference.once("value");
-      })
-      .then((snapshot) => {
-        console.log('Initialization successful with data snapshot', snapshot.val());
-        return res.send(snapshot.val());
+        console.log('Initialization processed');
+        return res.status(200).send(newUser);
       })
       .catch((error) =>
         {
